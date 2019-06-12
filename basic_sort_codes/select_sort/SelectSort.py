a6ee6e4db127c6e6594913faa0e9acd5935d6a5e
ktmtwm <agile@tanwenmin.top>
2019-06-12 16:41:54 +0800

    add select_sort to  basic_sort_codes init  select_sort TDD

---
+++ basic_sort_codes/select_sort/SelectSort.py
@@ -0,0 +1,7 @@
+"""
+"""
+
+class SelectSortObj(Object):
+	pass
+
+
---
+++ basic_sort_codes/select_sort/SelectSortTest.py
@@ -0,0 +1,10 @@
+"""
+"""
+from SelectSort import SelectSortObj
+import unittest
+
+class SelectSortTestCase(unittest.TestCase):
+	pass
+
+if __name__ == "__main__":
+	pass
