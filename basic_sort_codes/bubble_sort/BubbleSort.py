a4149753f9ff828c7685f03dd130503b29bf81e0
ktmtwm <agile@tanwenmin.top>
2019-06-14 16:35:05 +0800

    add desc bubble sort

--- basic_sort_codes/bubble_sort/BubbleSort.py
+++ basic_sort_codes/bubble_sort/BubbleSort.py
@@ -4,6 +4,14 @@
 class BubbleSortObj(object):
 	def sort(self, s):
 		"""
-		DESC: bubble sort
+		DESC: 	move the bigger one to right nearby,
+				when there is no move action return.
+
+				BETTER than select sort when sorted/part-sorted.
+		PERFORMANCE: 
+				o(n2),	average
+				o(n),	best
+				o(1),	space cost
 		"""
 		pass
+		
