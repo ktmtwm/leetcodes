67b98821de5f2d86696af0d658705827b38297a6
ktmtwm <agile@tanwenmin.top>
2019-06-14 15:22:13 +0800

    selectSort TDD, add unit case

--- basic_sort_codes/select_sort/SelectSortTest.py
+++ basic_sort_codes/select_sort/SelectSortTest.py
@@ -4,7 +4,50 @@ from SelectSort import SelectSortObj
 import unittest
 
 class SelectSortTestCase(unittest.TestCase):
-	pass
+	def setUp(self):
+		"""
+		DESC: init tested obj select_sort_obj
+		"""
+		self.select_sort = SelectSortObj()
+
+	def testEmptyList(self):
+		"""
+		DESC: sort empty list without error
+		"""
+		s = []
+		self.select_sort.sort(s)
+
+		self.assertEqual(s, [])
+
+	def testSortedList(self):
+		"""
+		DESC: normal sort
+		"""
+		s = [-1, 2, 2, 3, 4, 55]
+		self.select_sort.sort(s)
+
+		self.assertEqual(s, s)
+
+	def testReversedList(self):
+		"""
+		DESC: normal sort
+		"""
+		s = [55, 4, 3, 2, 2, -1]
+		self.select_sort.sort(s)
+
+		self.assertEqual(s, [1, 2, 2, 3, 4, 55])
+
+	def testLargeList(self):
+		pass
+
+def run_cases():
+	suite = unittest.TestSuite()
+	suite.addTest(SelectSortTestCase("testEmptyList"))
+	suite.addTest(SelectSortTestCase("testSortedList"))
+	suite.addTest(SelectSortTestCase("testReversedList"))
+	return suite
 
 if __name__ == "__main__":
-	pass
+	# unittest.main(defaultTest = 'run_cases')
+	runner = unittest.TextTestRunner()
+	runner.run(run_cases())
